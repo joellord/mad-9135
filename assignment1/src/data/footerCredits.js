e83967bc082bcbde2184a5db469aa8b1147ba4b8
Joel Lord <joelphy@gmail.com>
2017-09-12 19:52:13 -0400

    Added the assignment1 (obfuscated)

--- assignment1/src/data/footerCredits.js
+++
@@ -1,4 +0,0 @@
-let footerCredits = [
-    {key: 1, text: "Restaurant Logo by ", link: "http://freedesignfile.com/"},
-    {key: 2, text: "Menu items generated with ", link: "http://www.brooklynbarmenus.com/"}
-];
