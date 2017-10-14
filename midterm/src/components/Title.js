4b429691288cb649288ccf5f3dfaf2fbb83f3f24
Joel Lord <joelphy@gmail.com>
2017-10-14 14:03:59 -0400

    Removed source from the midterm

--- midterm/src/components/Title.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-export default class Title extends Component {
-    render() {
-        return (
-            <div className="column title">Contact List</div>
-        )
-    }
-}
